b88c2793b53fb3590d5a8e69a89aa5023d79c7b9
Vanshita <vanshitakhera17@gmail.com>
2024-07-04 16:07:11 +0530

    game board initialised

---
+++ doodlejump.js
@@ -0,0 +1,79 @@
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 576;
+let context;
+
+//doodler
+let doodlerWidth = 46;
+let doodlerHeight = 46;
+let doodlerX = boardWidth / 2 - doodlerWidth / 2;
+let doodlerY = (boardHeight * 7) / 8 - doodlerHeight;
+let doodlerRightImg;
+let doodlerLeftImg;
+
+//physics
+let velocityX = 0;
+
+let doodler = {
+  img: null,
+  x: doodlerX,
+  y: doodlerY,
+  width: doodlerWidth,
+  height: doodlerHeight,
+};
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d"); // used for drawing on the board
+
+  //draw doodler
+  // context.fillStyle = "green";
+  // context.fillRect(doodler.x, doodler.y, doodler.width, doodler.height);
+
+  //load images
+  doodlerRightImg = new Image();
+  doodlerRightImg.src = "./doodler-right.png";
+  doodler.img = doodlerRightImg;
+  doodlerRightImg.onload = function () {
+    context.drawImage(
+      doodler.img,
+      doodler.x,
+      doodler.y,
+      doodler.width,
+      doodler.height
+    );
+  };
+  doodlerLeftImg = new Image();
+  doodlerLeftImg.src = "./doodler-left.png";
+
+  requestAnimationFrame(update);
+  document.addEventListener("keydown", moveDoodler);
+};
+
+function update() {
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height)
+
+  //doodler over and over again
+  doodler.x += velocityX;
+  if (doodler.x > boardWidth) {
+    doodler.x = 0
+  }
+
+  context.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height);
+}
+
+function moveDoodler(e) {
+  if (e.code == "ArrowRight" || e.code == "KeyD") {
+    // move right
+    velocityX = 4;
+    doodler.img = doodlerRightImg;
+  } else if (e.code == "ArrowLeft" || e.code == "KeyA") {
+    //move left
+    velocityX = -4;
+    doodler.img = doodlerLeftImg;
+  }
+}
